12f4d51ae04beff9b0a776e046886523e1d17d10
ramya <rkramyakrishna561@gmail.com>
2020-06-13 14:33:48 +0530

    set up vuex state management

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card max-width="344" shaped>
+    <v-card-title>{{stock.name}}</v-card-title>
+    <v-card-subtitle>{{stock.price}}</v-card-subtitle>
+
+    <v-card-actions>
+      <v-text-field type="number" placeholder="Quantity" v-model.number="quantity"></v-text-field>
+      <v-btn rounded @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Buy</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+import store from './store/store';
 
+Vue.use(VueRouter);
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
